98bc67937c16c90edd423128b5bee30d3bfd1c50
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-31 01:29:01 -0700

    add context for token

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+
 import './Auth.css';
+import AuthContext from '../context/auth-context';
 
 class AuthPage extends Component {
     state = {
         isLogin: true
-    }
+    };
+
+    static contextType = AuthContext;
+
     constructor(props) {
         super(props);
         this.emailEl = React.createRef();
@@ -13,9 +18,9 @@ class AuthPage extends Component {
 
     switchModeHandler = () => {
         this.setState(prevState => {
-            return { isLogin: !prevState.isLogin }
-        })
-    }
+            return { isLogin: !prevState.isLogin };
+        });
+    };
 
     submitHandler = event => {
         event.preventDefault();
@@ -25,31 +30,31 @@ class AuthPage extends Component {
         if (email.trim().length === 0 || password.trim().length === 0) {
             return;
         }
+
         let requestBody = {
             query: `
-                query {
-                    login(email: "${email}", password: "${password}"){
-                        userId
-                        token
-                        tokenExpiration
-                    }
-                }
-            `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
         }
+      `
+        };
 
         if (!this.state.isLogin) {
             requestBody = {
                 query: `
-                    mutation {
-                        createUser(userInput: {email: "${email}", password: "${password}"}){
-                            _id
-                            email
-                        }
-                    }
-                `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `
             };
         }
-
 
         fetch('http://localhost:8000/graphql', {
             method: 'POST',
@@ -57,19 +62,27 @@ class AuthPage extends Component {
             headers: {
                 'Content-Type': 'application/json'
             }
-        }).then(res => {
-            if (res.status !== 200 || res.status !== 201) {
-                throw new Error('Failed')
-            }
-            return res.json();
         })
+            .then(res => {
+                if (res.status !== 200 && res.status !== 201) {
+                    throw new Error('Failed!');
+                }
+                return res.json();
+            })
             .then(resData => {
-                console.log(resData)
+                if (resData.data.login.token) {
+                    this.context.login(
+                        resData.data.login.token,
+                        resData.data.login.userId,
+                        resData.data.login.tokenExpiration
+                    );
+                }
             })
             .catch(err => {
-                console.log(err)
-            })
-    }
+                console.log(err);
+            });
+    };
+
     render() {
         return (
             <form className="auth-form" onSubmit={this.submitHandler}>
@@ -83,10 +96,12 @@ class AuthPage extends Component {
                 </div>
                 <div className="form-actions">
                     <button type="submit">Submit</button>
-                    <button type="button" onClick={this.switchModeHandler}>Switch to {this.state.isLogin ? 'Signup' : 'Login'}</button>
+                    <button type="button" onClick={this.switchModeHandler}>
+                        Switch to {this.state.isLogin ? 'Signup' : 'Login'}
+                    </button>
                 </div>
             </form>
-        )
+        );
     }
 }
 
