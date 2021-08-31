aacc2b7e3ee7fd5b31111152710926d73f36f05e
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-30 20:53:05 -0700

    add react setup

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import './App.css';
+import { Component } from 'react';
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+}
+
+export default App;
