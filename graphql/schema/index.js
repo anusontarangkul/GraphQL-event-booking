522bf8d8f375e55f3f74835731802f7b9249eb30
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-30 18:41:24 -0700

    refactor resolvers and helper functions

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -43,7 +43,7 @@ type RootQuery {
 type RootMutation {
     createEvent(eventInput:  EventInput): Event
     createUser(userInput: UserInput): User
-    bookEvent(eventId: ID): Booking!
+    bookEvent(eventId: ID!): Booking!
     cancelBooking(bookingId: ID!): Event!
 }
 schema {
