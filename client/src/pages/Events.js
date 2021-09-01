885534337f5d02ae2773044dae9f8db6709ab5a6
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-01 16:58:32 -0700

    update events

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -7,6 +7,15 @@ class Events extends Component {
     state = {
         creating: false
     }
+    constructor(props) {
+        super(props);
+        this.titleElRef = React.createRef();
+        this.priceElRef = React.createRef();
+        this.dateElRef = React.createRef();
+        this.descriptionElRef = React.createRef();
+
+    }
+
     startCreateEventHandler = () => {
         this.setState({ creating: true })
     }
@@ -21,13 +30,31 @@ class Events extends Component {
                 {this.state.creating && <Backdrop />}
                 {this.state.creating &&
                     <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
-                        <p>Modal Content</p>
+                        <form>
+                            <div className="form-control">
+                                <label htmlFor="title">Title</label>
+                                <input type="text" id="title" ref={this.titleElRef}></input>
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="price">Price</label>
+                                <input type="number" id="price" ref={this.priceElRef}></input>
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="date">Date</label>
+                                <input type="date" id="date" ref={this.dateElRef}></input>
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="description">Description</label>
+                                <textarea id="description" rows="4" ref={this.descriptionElRef}></textarea>
+                            </div>
+                        </form>
                     </Modal>
                 }
                 <div className="events-control">
                     <p>Share your own Events!</p>
                     <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
                 </div>
+
             </React.Fragment>
         )
     }
