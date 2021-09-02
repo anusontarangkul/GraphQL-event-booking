a6eabfc0bd8f41ac3dc1b874bea14ff04c3477b1
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-01 23:37:40 -0700

    cancel bookings

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -1,18 +1,17 @@
 const Event = require('../../models/event');
 const Booking = require('../../models/booking');
-const { transformEvent, transformBooking } = require('./merge')
+const { transformBooking, transformEvent } = require('./merge');
 
 module.exports = {
-
     bookings: async (args, req) => {
         if (!req.isAuth) {
             throw new Error('Unauthenticated!');
         }
         try {
-            const bookings = await Booking.find();
+            const bookings = await Booking.find({ user: req.userId });
             return bookings.map(booking => {
-                return transformBooking(booking)
-            })
+                return transformBooking(booking);
+            });
         } catch (err) {
             throw err;
         }
@@ -23,7 +22,7 @@ module.exports = {
         }
         const fetchedEvent = await Event.findOne({ _id: args.eventId });
         const booking = new Booking({
-            user: '6129a29d23ecd562449ddb5b',
+            user: req.userId,
             event: fetchedEvent
         });
         const result = await booking.save();
@@ -34,11 +33,12 @@ module.exports = {
             throw new Error('Unauthenticated!');
         }
         try {
-            const booking = await Booking.deleteOne({ _id: args.bookingId });
-            const event = transformEvent(booking.event)
-            return event
+            const booking = await Booking.findById(args.bookingId).populate('event');
+            const event = transformEvent(booking.event);
+            await Booking.deleteOne({ _id: args.bookingId });
+            return event;
         } catch (err) {
-            throw err
+            throw err;
         }
     }
 };
